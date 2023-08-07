62cbdaec24496fc05dc2fad95a24e9b9f9a34b4c
alphiree <rylupague@gmail.com>
2023-08-07 10:56:51 +0800

    Modified Libraries

--- basic_packages.py
+++ basic_packages.py
@@ -13,35 +13,8 @@ import seaborn as sns
 ## For Math
 import math
 
-## For image and video input
-import cv2
-
-# The shutil in Python is a module that offers several functions to deal with operations on files and their collections. It provides the ability to copy and removal of files.
-# In a way, it is similar to the OS Module; however, the OS Module does have functions dealing with collections of files.
-import shutil
-
-## Splitting folders
-import splitfolders
-
-## Python Imaging Library
-import PIL
-import PIL.Image
-
-## The requests module allows you to send HTTP requests using Python.
-import requests
-
-## Time module
-import time
-from datetime import datetime
-
-# System libraries
-
-## For acessing the directory
-import os
-
 ## For Randomization
 import random
-
 
 ## Regular expression
 ## A regular expression (or RE) specifies a set of strings that matches it
